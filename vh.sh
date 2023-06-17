8b2fe32a5b07e041c8fe1fb4910fc200b8b77c92
noou <83199729+nooufiy@users.noreply.github.com>
2023-06-17 20:43:44 +0700

    Update vh.sh

--- vh.sh
+++ vh.sh
@@ -78,7 +78,7 @@ while true; do
         mysql -u root -p"$pw" -e "FLUSH PRIVILEGES;"
 
         wget -P "$home_dir/$domain" https://wordpress.org/latest.tar.gz
-        tar -zxvf "$home_dir/$domain/latest.tar.gz" -C "$home_dir/$domain"
+        tar -zxvf "$home_dir/$domain/latest.tar.gz" -C "$home_dir/$domain" --strip-components=1
         # tar -zxvf "$home_dir/$domain/latest.tar.gz" --directory "$home_dir/$domain"
         # mv "$home_dir/$domain/wordpress/*" "$home_dir/$domain"
         cp "$home_dir/$domain/wp-config-sample.php" "$home_dir/$domain/wp-config.php"
