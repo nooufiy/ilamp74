08e595865d8f3f3232bfeed894217c892e1c235e
noou <83199729+nooufiy@users.noreply.github.com>
2023-06-17 22:00:38 +0700

    Update vh.sh

--- vh.sh
+++ vh.sh
@@ -84,9 +84,9 @@ while true; do
             # tar -zxvf "$home_dir/$domain/latest.tar.gz" --directory "$home_dir/$domain"
             # mv "$home_dir/$domain/wordpress/*" "$home_dir/$domain"
             cp "$home_dir/$domain/wp-config-sample.php" "$home_dir/$domain/wp-config.php"
-            sed -i "s/database_name_here/dbname/g" "$home_dir/$domain/wp-config.php"
-            sed -i "s/username_here/dbuser/g" "$home_dir/$domain/wp-config.php"
-            sed -i "s/password_here/dbpass/g" "$home_dir/$domain/wp-config.php"
+            sed -i "s/database_name_here/$dbname/g" "$home_dir/$domain/wp-config.php"
+            sed -i "s/username_here/$dbuser/g" "$home_dir/$domain/wp-config.php"
+            sed -i "s/password_here/$dbpass/g" "$home_dir/$domain/wp-config.php"
 
             #set WP salts
             perl -i -pe'
