772b8abbe567f224acb12061abfaa2bfebc1b891
noou <83199729+nooufiy@users.noreply.github.com>
2023-06-25 01:23:35 +0700

    Update vh.sh

--- vh.sh
+++ vh.sh
@@ -30,104 +30,109 @@ while true; do
 
     # Memeriksa apakah ada perubahan pada daftar domain/subdomain
     if [[ ! -z "${domain_list[*]}" ]]; then
+        new_domains=()
+
         # Loop untuk setiap domain/subdomain
         for domain in "${domain_list[@]}"; do
-        # Memeriksa apakah domain belum diproses sebelumnya
-        if ! grep -q "$domain" "$processed_file"; then
-            # Menulis konfigurasi virtual host ke sites.conf
-            dot_count=$(grep -o "\." <<< "$domain" | wc -l)
-            if [[ dot_count -eq 1 ]]; then
-            write_to_sites_conf "$domain" "domain"
-            elif [[ dot_count -eq 2 ]]; then
-            write_to_sites_conf "$domain" "subdomain"
+            if ! grep -q "$domain" "$processed_file"; then
+                new_domains+=("$domain") # Menambahkan domain yang belum dieksekusi ke dalam array new_domains
             fi
-
-            mkdir "$home_dir/$domain"
-            timestamp=$(date +%s)
-            short=$(echo "$domain" | sed 's/\.//g' | cut -c 1-5)
-            rand_chars=$(head /dev/urandom | tr -dc 'a-z' | fold -w 3 | head -n 1)
-            dbuser="${short}_usr_${rand_chars}"
-            dbname="${short}_nam_${rand_chars}"
-
-            dbpass="${short}_pas_${timestamp}"
-
-            pw=""
-
-            if ! mysql -u root -p"$pw" -e "SELECT COUNT(*) FROM mysql.user WHERE user = '$dbuser';" | grep -q '1'; then
-              mysql -u root -p"$pw" -e "CREATE USER '$dbuser'@'localhost' IDENTIFIED BY '$dbpass';"
-            fi
-
-            if mysql -u root -p"$pw" -e "USE $dbname;" ; then
-                mysql -u root -p"$pw" -e "DROP DATABASE $dbname;"
-            fi
-
-            mysql -u root -p"$pw" -e "CREATE DATABASE IF NOT EXISTS $dbname;"
-            mysql -u root -p"$pw" -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'localhost';"
-            mysql -u root -p"$pw" -e "FLUSH PRIVILEGES;"
-
-            wget -P "$home_dir/$domain" https://wordpress.org/latest.tar.gz
-            tar -zxvf "$home_dir/$domain/latest.tar.gz" -C "$home_dir/$domain" --strip-components=1
-            cp "$home_dir/$domain/wp-config-sample.php" "$home_dir/$domain/wp-config.php"
-            sed -i "s/database_name_here/$dbname/g" "$home_dir/$domain/wp-config.php"
-            sed -i "s/username_here/$dbuser/g" "$home_dir/$domain/wp-config.php"
-            sed -i "s/password_here/$dbpass/g" "$home_dir/$domain/wp-config.php"
-
-            perl -i -pe'
-            BEGIN {
-                @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
-                push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
-                sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
-            }
-            s/put your unique phrase here/salt()/ge
-            ' "$home_dir/$domain/wp-config.php"
-
-            [ ! -d "$home_dir/$domain/wp-content/uploads" ] && mkdir "$home_dir/$domain/wp-content/uploads"
-
-            cd "$home_dir/$domain"
-
-            htawp="# BEGIN WordPress\n\n\
-            RewriteEngine On\n\
-            RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]\n\
-            RewriteBase /\n\
-            RewriteRule ^index\.php$ - [L]\n\
-            RewriteCond %{REQUEST_FILENAME} !-f\n\
-            RewriteCond %{REQUEST_FILENAME} !-d\n\
-            RewriteRule . /index.php [L]\n\n\
-            # END WordPress\n"
-            echo -e "$htawp" > "$home_dir/$domain/.htaccess"
-
-            wp core install --url="http://$domain/" --title="$domain" --admin_user="admin" --admin_password=rahasi4a911* --admin_email="$email" --allow-root
-            wp option update blogdescription "" --allow-root
-            wp rewrite structure '/%postname%/' --hard --allow-root
-
-            chown -R apache:apache "$home_dir/$domain"
-            # chmod -R 755 "$home_dir/$domain"
-            chcon -R system_u:object_r:httpd_sys_content_t "$home_dir/$domain"
-
-            if certbot certificates | grep -q "Expiry Date"; then
-                echo "Sertifikat ada."
-            else
-                echo "Sertifikat tidak ada atau sudah expired."
-                certbot --apache -d "$domain" --email "$email" --agree-tos -n
-            fi
-
-            echo "$domain,$dbuser,$dbname,$dbpass" >> "$processed_file"
-        fi
         done
-      service httpd graceful
+
+        if [[ ! -z "${new_domains[*]}" ]]; then
+            # echo "Domain baru yang akan dieksekusi:"
+            for newdomain in "${new_domains[@]}"; do
+                # Menulis konfigurasi virtual host ke sites.conf
+                dot_count=$(grep -o "\." <<< "$newdomain" | wc -l)
+                if [[ dot_count -eq 1 ]]; then
+                write_to_sites_conf "$newdomain" "domain"
+                elif [[ dot_count -eq 2 ]]; then
+                write_to_sites_conf "$newdomain" "subdomain"
+                fi
+
+                mkdir "$home_dir/$newdomain"
+                timestamp=$(date +%s)
+                short=$(echo "$newdomain" | sed 's/\.//g' | cut -c 1-5)
+                rand_chars=$(head /dev/urandom | tr -dc 'a-z' | fold -w 3 | head -n 1)
+                dbuser="${short}_usr_${rand_chars}"
+                dbname="${short}_nam_${rand_chars}"
+                dbpass="${short}_pas_${timestamp}"
+
+                pw=""
+
+                if ! mysql -u root -p"$pw" -e "SELECT COUNT(*) FROM mysql.user WHERE user = '$dbuser';" | grep -q '1'; then
+                  mysql -u root -p"$pw" -e "CREATE USER '$dbuser'@'localhost' IDENTIFIED BY '$dbpass';"
+                fi
+
+                if mysql -u root -p"$pw" -e "USE $dbname;" ; then
+                    mysql -u root -p"$pw" -e "DROP DATABASE $dbname;"
+                fi
+
+                mysql -u root -p"$pw" -e "CREATE DATABASE IF NOT EXISTS $dbname;"
+                mysql -u root -p"$pw" -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'localhost';"
+                mysql -u root -p"$pw" -e "FLUSH PRIVILEGES;"
+
+                wget -P "$home_dir/$newdomain" https://wordpress.org/latest.tar.gz
+                tar -zxvf "$home_dir/$newdomain/latest.tar.gz" -C "$home_dir/$newdomain" --strip-components=1
+                cp "$home_dir/$newdomain/wp-config-sample.php" "$home_dir/$newdomain/wp-config.php"
+                sed -i "s/database_name_here/$dbname/g" "$home_dir/$newdomain/wp-config.php"
+                sed -i "s/username_here/$dbuser/g" "$home_dir/$newdomain/wp-config.php"
+                sed -i "s/password_here/$dbpass/g" "$home_dir/$newdomain/wp-config.php"
+
+                perl -i -pe'
+                BEGIN {
+                    @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
+                    push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
+                    sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
+                }
+                s/put your unique phrase here/salt()/ge
+                ' "$home_dir/$newdomain/wp-config.php"
+
+                [ ! -d "$home_dir/$newdomain/wp-content/uploads" ] && mkdir "$home_dir/$newdomain/wp-content/uploads"
+
+                cd "$home_dir/$newdomain"
+
+                htawp="# BEGIN WordPress\n\n\
+                RewriteEngine On\n\
+                RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]\n\
+                RewriteBase /\n\
+                RewriteRule ^index\.php$ - [L]\n\
+                RewriteCond %{REQUEST_FILENAME} !-f\n\
+                RewriteCond %{REQUEST_FILENAME} !-d\n\
+                RewriteRule . /index.php [L]\n\n\
+                # END WordPress\n"
+                echo -e "$htawp" > "$home_dir/$newdomain/.htaccess"
+
+                wp core install --url="http://$newdomain/" --title="$newdomain" --admin_user="admin" --admin_password=rahasi4a911* --admin_email="$email" --allow-root
+                wp option update blogdescription "" --allow-root
+                wp rewrite structure '/%postname%/' --hard --allow-root
+
+                chown -R apache:apache "$home_dir/$newdomain"
+                # chmod -R 755 "$home_dir/$newdomain"
+                chcon -R system_u:object_r:httpd_sys_content_t "$home_dir/$newdomain"
+
+                if certbot certificates | grep -q "Expiry Date"; then
+                    echo "Sertifikat ada."
+                else
+                    echo "Sertifikat tidak ada atau sudah expired."
+                    certbot --apache -d "$newdomain" --email "$email" --agree-tos -n
+                fi
+
+                echo "$newdomain,$dbuser,$dbname,$dbpass" >> "$processed_file"
+            done
+            service httpd graceful
+            
+            ssl_dir="/etc/letsencrypt"
+            backup_file="ssl_backup_$(date +%Y%m%d).tar.gz"
+            tar -czvf "$sslbekup/$backup_file" "$ssl_dir"
+
+            # Hapus backup lama (lebih dari 3 hari)
+            old_backups=$(find "$sslbekup" -name "ssl_backup_*.tar.gz" -type f -mtime +3)
+            if [[ -n $old_backups ]]; then
+                rm -f $old_backups
+            fi
+        fi
     fi
   fi
-
-
-  ssl_dir="/etc/letsencrypt"
-  backup_file="ssl_backup_$(date +%Y%m%d).tar.gz"
-  tar -czvf "$sslbekup/$backup_file" "$ssl_dir"
-
-  # Hapus backup lama (lebih dari 3 hari)
-  old_backups=$(find "$sslbekup" -name "ssl_backup_*.tar.gz" -type f -mtime +3)
-  if [[ -n $old_backups ]]; then
-      rm -f $old_backups
-  fi
-
   sleep 20
 done
