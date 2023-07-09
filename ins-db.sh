b1e0bff28ab32891ad2d38d4400ddbc837771e91
noou <83199729+nooufiy@users.noreply.github.com>
2023-07-09 14:28:39 +0700

    Update ins-db.sh

--- ins-db.sh
+++ ins-db.sh
@@ -257,7 +257,9 @@ fi
 #ssh2
 # yum install -y libssh2 libssh2-devel gcc make php-devel
 yum install -y libssh2 libssh2-devel make
-pecl install ssh2
+# pecl install ssh2
+yum install php-ssh2
+
 # pecl install ssh2-1.3.1
 # echo "extension=ssh2.so" | sudo tee /etc/php.d/ssh2.ini
 # echo "extension=ssh2.so" >> /etc/php.d/ssh2.ini
