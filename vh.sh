220122a4a653c6d4d9633fdfcc195ca339881cf3
noou <83199729+nooufiy@users.noreply.github.com>
2023-12-05 13:51:37 +0700

    Update vh.sh

--- vh.sh
+++ vh.sh
@@ -48,7 +48,7 @@ while true; do
 
                     # Cek keberadaan file domain1.txt
                     if [ ! -f "$rundir/active/$newdomain.txt" ]; then
-                        screen -dmS "$domain" sh setdom.sh "$newdtdom"
+                        screen -dmS "$newdomain" sh setdom.sh "$newdtdom"
                     fi
                 done
 
