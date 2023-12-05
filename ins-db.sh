d39a231312d8b79e7a60b7c4cc369f0535cf1017
noou <83199729+nooufiy@users.noreply.github.com>
2023-12-05 09:43:18 +0700

    Update ins-db.sh

--- ins-db.sh
+++ ins-db.sh
@@ -215,7 +215,7 @@ if [ "$vhs" == "manual" ]; then
   cat <<EOF | sudo tee -a "$ds/cnf.txt" >/dev/null
 email=$mail
 sites_conf_dir=/etc/httpd/conf.s
-sites_conf=/$sites_conf_dir/sites.conf
+sites_conf=/\$sites_conf_dir/sites.conf
 home_dir=$dpub/w
 home_dt=$dpub/d
 processed_file=$ds/processed_domains.txt
